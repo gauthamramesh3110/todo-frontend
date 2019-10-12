02f9db38b09807e8d312477426b82a315e790dc2
Gautham Ramesh Babu <rgautham3110@gmail.com>
2019-10-12 15:12:45 +0530

    Added Main Activity in blank state

---
+++ app/src/main/java/com/netlify/toucantodo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.netlify.toucantodo;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
